2a76090e1b52e1719cf45db04fd3283ec38751f1
Piyush <piyushgurjar0725@gmail.com>
2024-08-01 22:38:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -78,6 +78,9 @@ app.use((req, res, next) => {
 });
 
 // Routes configuration
+app.get('/', (req, res) => {
+    res.redirect('/listings');
+});
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
